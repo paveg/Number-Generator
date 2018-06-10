4a266bff64595185bf5052a7e3fd72c279d4f13f
Ryota Ikezawa <pavegy@gmail.com>
2018-06-10 10:49:10 +0900

    clean up index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var canvas, ctx , textbox;
+var canvas, ctx, textbox;
 
 window.onload = function () {
   canvas = document.getElementById("canvas");
@@ -14,14 +14,14 @@ function saveImage() {
   ctx = canvas_save.getContext('2d');
   ctx.font = '1760px electricboots';
   ctx.lineJoin = 'round';
-  ctx.setTransform(1,0,0,1,0,0);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.clearRect(0, 0, canvas_save.width, canvas_save.height);
-  ctx.setTransform(1,0,-0.1,1,0,0);
+  ctx.setTransform(1, 0, -0.1, 1, 0, 0);
   var posx = 320;
   var posy = 1500;
   var text = textbox.value;
   ctx.strokeStyle = "#000";
-  ctx.lineWidth =0;
+  ctx.lineWidth = 0;
   ctx.fillText(text, posx + 4, posy + 4);
 
   var a = document.createElement("a");
@@ -33,14 +33,14 @@ function saveImage() {
 }
 
 function redraw() {
-  ctx.setTransform(1,0,0,1,0,0);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.setTransform(1,0,-0.18,1,0,0);
+  ctx.setTransform(1, 0, -0.18, 1, 0, 0);
   var posx = 70;
   var posy = 100;
   var text = textbox.value;
 
   ctx.strokeStyle = "#000";
-  ctx.lineWidth =0;
+  ctx.lineWidth = 0;
   ctx.fillText(text, posx + 4, posy + 4);
 }
